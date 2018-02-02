91bab02da09e61b61d78155a0f3f2ddc97de2418
JKnights <james@i-studio.co.uk>
2018-02-02 11:21:15 +0000

    - changed springboot for war packaging

--- src/main/java/com/jamesknights/application/Application.java
+++ src/main/java/com/jamesknights/application/Application.java
@@ -8,6 +8,8 @@ package com.jamesknights.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -16,9 +18,14 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories("com.jamesknights.common.user.repository")
 @ComponentScan(basePackages = { "com.jamesknights.*"})
 @EntityScan("com.jamesknights.*") 
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
+     @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
 }
